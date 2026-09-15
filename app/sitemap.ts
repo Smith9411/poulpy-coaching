35b8f2df41fb104168541d4e081cbfcd4801aefb
Smith9411 <smith9411@users.noreply.github.com>
2026-09-15 18:42:01 +0200

    feat(seo): add Google Site Name WebSite schema, OpenGraph tags, robots.txt and sitemap.xml to display Poulpy Coaching and custom icon on Google

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://poulpy-coaching.vercel.app';
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/avis`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ];
+}
